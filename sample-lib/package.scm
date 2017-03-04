7a9afd0710cd8cb48edd60dc15ebf6e7e32456b9
Justin Ethier <justin.ethier@gmail.com>
2017-03-04 18:38:35 -0500

    WIP

--- sample-lib/package.scm
+++ sample-lib/package.scm
@@ -9,10 +9,12 @@
 (conflicts)
 (replaces)
 ;; List of files to compile and install
-(files '(cyclone sample.sld))
+;; Can list each filename as a string (EG: "cyclone/sample.sld)
+;; or as the installed library name (sld files only)
+(files (cyclone sample) "noone.scm")
 ;; Custom install commands
 (install "")
 ;; Custom uninstall commands
 (uninstall "")
 ;; Test files to build and execute
-(tests '(tests test.scm))
+(tests (tests test.scm))
