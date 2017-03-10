439084c0cbfd72e54e4be2ad2bc674028df287d7
Justin Ethier <justin.ethier@gmail.com>
2017-03-10 04:46:58 -0500

    Fixed up repo dir

--- create-index.sh
+++ create-index.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #REPODIR=../repo
-REPODIR=../repo
+REPODIR=repo
 INDEX=${REPODIR}/index.dat
 mkdir -p $REPODIR
 rm -f ${INDEX}
 touch ${INDEX}
 
 for D in *; do
-    #if [ -d "${D}" ] && [ "${D}" -ne "${REPODIR}" ]; then
+    if [ -d "${D}" ] && [ "${D}" != "${REPODIR}" ]; then
     #if [[ -d "${D}" && "${D}" -ne "${REPODIR}" ]]; then
-    if [ -d "${D}" ]; then
+    #if [ -d "${D}" ]; then
         echo "${D}"   # your processing here
         tar -cf "${D}".tar "${D}" && gzip -f "${D}".tar
         VERSION=`./get-ver "${D}"/package.scm`
         #echo $VERSION
-        mv "${D}".tar.gz "${D}"-"${VERSION}".tar.gz
+        mv "${D}".tar.gz ${REPODIR}/"${D}"-"${VERSION}".tar.gz
         echo "( ${D} ${VERSION} ${D}-${VERSION}.tar.gz )" >> ${INDEX}
     fi
 done
