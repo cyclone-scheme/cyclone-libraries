756a3f0d9a32fa6cbcacd19c4058f3afa425ae76
Justin Ethier <justin.ethier@gmail.com>
2017-03-10 04:44:14 -0500

    WIP

--- create-index.sh
+++ create-index.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+#REPODIR=../repo
+REPODIR=../repo
+INDEX=${REPODIR}/index.dat
+mkdir -p $REPODIR
+rm -f ${INDEX}
+touch ${INDEX}
+
 for D in *; do
+    #if [ -d "${D}" ] && [ "${D}" -ne "${REPODIR}" ]; then
+    #if [[ -d "${D}" && "${D}" -ne "${REPODIR}" ]]; then
     if [ -d "${D}" ]; then
-        #echo "${D}"   # your processing here
+        echo "${D}"   # your processing here
         tar -cf "${D}".tar "${D}" && gzip -f "${D}".tar
         VERSION=`./get-ver "${D}"/package.scm`
         #echo $VERSION
         mv "${D}".tar.gz "${D}"-"${VERSION}".tar.gz
+        echo "( ${D} ${VERSION} ${D}-${VERSION}.tar.gz )" >> ${INDEX}
     fi
 done
