0088ce8ad9934b133417b0b7c68c1291e605c874
Justin Ethier <justin.ethier@gmail.com>
2017-03-11 17:52:29 -0500

    cleanup

--- sample-lib/package.scm
+++ sample-lib/package.scm
@@ -12,17 +12,23 @@
   "cyclone main.scm"
   "cyclone cyclone/sample.sld")
 
+;; There are several aliases used in the next 2 directives:
+;; ~BIN~  - Directory for binaries
+;; ~DATA~ - Directory for scheme libraries to be installed 
+;; ~LIB~ - Directory for C libraries (not normally used)
+;; ~INCLUDE~ - Directory for C headers (not normally used)
 (install 
-  "mkdir -p ~DESTDIR~/bin"
-  "install -m0755 main ~DESTDIR~/bin"
-  "mkdir -p ~DESTDIR~/~LIB~/cyclone"
-  "install -m0644 cyclone/sample.sld ~DESTDIR~/~LIB~/cyclone"
-  "install -m0644 cyclone/sample.o ~DESTDIR~/~LIB~/cyclone"
-  "install -m0644 cyclone/sample.meta ~DESTDIR~/~LIB~/cyclone"
+  "mkdir -p ~BIN~"
+  "install -m0755 main ~BIN~/"
+  "mkdir -p ~DATA~/cyclone"
+  "install -m0644 cyclone/sample.sld ~DATA~/cyclone"
+  "install -m0644 cyclone/sample.o ~DATA~/cyclone"
+  "install -m0644 cyclone/sample.meta ~DATA~/cyclone"
   )
 
 ;; Custom uninstall commands
-(uninstall "echo 'uninstall cmd 1'"
-           "echo 'uninstall cmd 2'")
+(uninstall 
+  "echo 'uninstall cmd 1'"
+  "echo 'uninstall cmd 2'")
 ;; Test files to build and execute
 (test "cyclone tests/test.scm && ./tests/test")
