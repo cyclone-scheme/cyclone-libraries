b64f061c8a2af429cf7bf7e472632f79bbf25abc
Justin Ethier <justin.ethier@gmail.com>
2017-03-11 17:28:17 -0500

    Add full info to index.dat

--- create-index.sh
+++ create-index.sh
@@ -10,8 +10,9 @@ touch ${INDEX}
 for D in *; do
     if [ -d "${D}" ] && [ "${D}" != "${REPODIR}" ]; then
         VERSION=`./query-pkg "${D}"/package.scm ver`
+        INFO=`./query-pkg "${D}"/package.scm`
         tar -cf "${D}".tar "${D}" && gzip -f "${D}".tar
         mv "${D}".tar.gz ${REPODIR}/"${D}"-"${VERSION}".tar.gz
-        echo "( ${D} ${VERSION} ${D}-${VERSION}.tar.gz )" >> ${INDEX}
+        echo "( ${D} ${VERSION} ${D}-${VERSION}.tar.gz ${INFO} )" >> ${INDEX}
     fi
 done
